8f2942269070215925102e9a09d51811532b1e41
Igor <igor.ptz@hotmail.com>
2021-11-23 23:13:35 -0300

    Update

---
+++ lab-4-adjective/src/test/java/lab/four/client/adjective/Lab4AdjectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.four.client.adjective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4AdjectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
